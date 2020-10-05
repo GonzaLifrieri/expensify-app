0be511973176848ab9f94b590e4a052de32cf2f9
GonzaLifrieri <gonza.lifrieri@gmail.com>
2020-10-05 19:05:51 -0300

    Fetch Expenses Added

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -69,3 +69,12 @@ test('should edit an expense', () => {
     const state = expensesReducer(expenses, action);
     expect(state[1].amount).toBe(amount);
 });
+
+test('should set expenses', () => {
+    const action = {
+        type: 'SET_EXPENSES',
+        expenses: [expenses[1]]
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[1]]);
+});
