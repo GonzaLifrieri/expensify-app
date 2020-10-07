e4b20e5715fc0385279f635516fa16c8ea602aba
GonzaLifrieri <gonza.lifrieri@gmail.com>
2020-10-07 18:57:57 -0300

    Add Expensily styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Expensily</h1>
+            <p>Es tiempo de ordenar tus gastos de la mejor forma</p>
+            <button className="button" onClick={startLogin}>Entrar con Google</button>
+        </div>
     </div>
 );
 
--- src/components/expenseList.js
+++ src/components/expenseList.js
@@ -4,16 +4,25 @@ import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from './../selectors/expenses';
 
 export const ExpenseList = (props) => (
-    <div>
-        {
-            props.expenses.length === 0 ? (
-                <p>No expenses added</p>
-            ) : (
-                    props.expenses.map((expense) => {
-                        return <ExpenseListItem key={expense.id} {...expense} />
-                    })
-                )
-        }
+    <div className="content-container">
+        <div className="list-header">
+            <div className="show-for-mobile">Expensas</div>
+            <div className="show-for-desktop">Expensa</div>
+            <div className="show-for-desktop">Monto</div>
+        </div>
+        <div className="list-body">
+            {
+                props.expenses.length === 0 ? (
+                    <div className="list-item list-item--message">
+                        <span>Sin gastos</span>
+                    </div>
+                ) : (
+                        props.expenses.map((expense) => {
+                            return <ExpenseListItem key={expense.id} {...expense} />
+                        })
+                    )
+            }
+        </div>
     </div>
 );
 
